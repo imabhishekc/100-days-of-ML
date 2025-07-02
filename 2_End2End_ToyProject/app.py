5ab7cd478f3b8434d2cab23f9fc70f863a6f80dd
imabhishekc <abhishekchakraborty776@gmail.com>
2025-07-02 20:46:28 +0545

    Adding streamlit UI

--- 2_End2End_ToyProject/app.py
+++ 2_End2End_ToyProject/app.py
@@ -1,28 +1,31 @@
 import streamlit as st
 import pickle
+import numpy as np
 
 # Load the trained model
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Set the page title and header
+# App title and description
 st.title('Student Placement Prediction')
-st.header('Enter Student Details')
+st.write('Enter the student\'s CGPA and IQ to predict whether they will be placed.')
 
-# Create input fields for IQ and CGPA
-iq = st.number_input('IQ')
-cgpa = st.number_input('CGPA')
+# Input fields for CGPA and IQ
+cgpa = st.number_input('CGPA', min_value=0.0, max_value=9.0, step=0.1)
+iq = st.number_input('IQ', min_value=0, max_value=200, step=1)
 
-# Create a button to make predictions
+# Prediction button
 if st.button('Predict'):
-    # Prepare the input data for the model
-    input_data = [[iq, cgpa]]
+    # Create a numpy array from the inputs
+    input_data = np.array([[cgpa, iq]])
 
-    # Make a prediction
+    # Make prediction
     prediction = model.predict(input_data)
 
-    # Display the prediction
-    if prediction[0] == 1:
-        st.success('This student is likely to get placed.')
+    # Display the result
+    if prediction[0] == 1.0:
+        st.success('The student will be placed!')
     else:
-        st.error('This student is not likely to get placed.')
+        st.error('The student will not be placed.')
+
+st.sidebar.info("This is a web app to predict whether a student will get placed based on their CGPA and IQ.")
